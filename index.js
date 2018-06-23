b9e941a30ec30669701c75ae7d6e6530cf149c67
Peter Traversa <peter.traversa@gmail.com>
2018-06-23 19:25:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`;
 }
 
@@ -13,4 +14,15 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
+=======
+  return `Happy holidays, ${name}`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`;
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}`;
+>>>>>>> 328bc7a17d8431006f20c65fac73989deffe4fef
 }
